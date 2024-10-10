2b03b7a13cafdf2a3772d73f4f8ee2d76351d2f6
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-10-10 09:04:53 -0700

    impl AppDistances class

---
+++ lib/global/utils/constants/app_distances.dart
@@ -0,0 +1,13 @@
+class AppDistances {
+  AppDistances._();
+  static const small2 = 2.0;
+  static const small4 = 4.0;
+  static const small8 = 8.0;
+
+  static const medium12 = 12.0;
+  static const medium16 = 16.0;
+
+  static const large = 24.0;
+
+  static const pageBdyMargin = 20.0;
+}
