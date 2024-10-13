50e620de1994757d788bed82db24cb0207688a2f
amirrezahaqi <amirrezahaqi@gmail.com>
2024-10-13 11:28:46 +0330

    home games cards

--- lib/global/widgets/home_games_cards.dart
+++ lib/global/widgets/home_games_cards.dart
@@ -16,50 +16,47 @@ class HomeGamesCards extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Padding(
-      padding: const EdgeInsets.only(
-          right: AppDistances.large, left: AppDistances.large),
-      child: SizedBox(
-        width: size.width / 4,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: size.width / 3.5,
+    return SizedBox(
+      width: size.width / 3.5,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: size.width / 3,
+            child: Container(
+              decoration: BoxDecoration(
+                color: UiColors.darkBlueColor3,
+                borderRadius: BorderRadius.circular(AppDistances.large),
+              ),
               child: Container(
+                margin: const EdgeInsets.only(bottom: 5),
                 decoration: BoxDecoration(
-                  color: UiColors.darkBlueColor3,
+                  color: UiColors.lightBlueColor,
                   borderRadius: BorderRadius.circular(AppDistances.large),
                 ),
-                child: Container(
-                  margin: const EdgeInsets.only(bottom: 5),
-                  decoration: BoxDecoration(
-                    color: UiColors.lightBlueColor,
-                    borderRadius: BorderRadius.circular(AppDistances.large),
-                  ),
-                  child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                          borderRadius:
-                              BorderRadius.circular(AppDistances.large),
-                        ),
-                        backgroundColor: UiColors.lightBlueColor3,
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(AppDistances.large),
                       ),
-                      onPressed: () => onPress(),
-                      child: Image.asset(
-                        imageAsset,
-                        scale: 1,
-                      )),
-                ),
+                      backgroundColor: UiColors.lightBlueColor3,
+                    ),
+                    onPressed: () => onPress(),
+                    child: Image.asset(
+                      imageAsset,
+                      width: size.width / 3,
+                      height: size.width / 5,
+                      fit: BoxFit.fill,
+                    )),
               ),
             ),
-            Text(
-              btnText,
-              style: const TextStyle(
-                  color: UiColors.whiteColor, fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
+          ),
+          Text(
+            btnText,
+            style: const TextStyle(
+                color: UiColors.whiteColor, fontWeight: FontWeight.bold),
+          ),
+        ],
       ),
     );
   }
