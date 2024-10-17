592b549a8351b476333092dba27eb38fe8c8b619
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-10-17 11:27:00 -0700

    impl BottomSheetBodyWidget custom widget

--- lib/modules/home/view/home_view.dart
+++ lib/modules/home/view/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_new/global/widgets/app-bar.dart';
+import 'package:flutter_application_new/global/widgets/bottom_sheet_body_widget.dart';
 import 'package:flutter_application_new/global/widgets/dialog_body_widget.dart';
 import 'package:flutter_application_new/global/widgets/main_btn.dart';
 import 'package:flutter_application_new/global/widgets/player_counter_widget.dart';
@@ -14,24 +15,23 @@ class HomeView extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
-        appBar: PreferredSize(
-            preferredSize: Size.fromHeight(size.height / 12),
-            child: const AppBarWidget()),
-        body: SizedBox(
-          width: double.infinity,
-            height: double.infinity,
-            child: Center(child: Padding(
-              padding: const EdgeInsets.all(20),
-              child: MainButton(btnText: "open",onPress: (){
-                showDialog(
-                    context: context,
-                    barrierDismissible: true,
-                    builder: (context) => DialogBodyWidget(dialogBody: [
-                      PlayerCounterWidget(playerCounts: [2,8,4,5,6,3], onItemTap: (number) {},)
-                    ],));
-              }),
-            )))
-      ),
+          appBar: PreferredSize(
+              preferredSize: Size.fromHeight(size.height / 12),
+              child: const AppBarWidget()),
+          body: SizedBox(
+              width: double.infinity,
+              height: double.infinity,
+              child: Center(
+                  child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: MainButton(
+                    btnText: "open",
+                    onPress: () => buildShowCustomModalBottomSheet(
+                          context: context,
+                          builder: (context) =>
+                              const BottomSheetBodyWidget(children: []),
+                        )),
+              )))),
     );
   }
 }
