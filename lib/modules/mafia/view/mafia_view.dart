a21ceb5309eb78d46ad5231dc4e1368eebce11d3
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-11-05 09:31:50 -0800

    connect mafia cubit to mafia view

--- lib/modules/mafia/view/mafia_view.dart
+++ lib/modules/mafia/view/mafia_view.dart
@@ -6,8 +6,8 @@ import 'package:flutter_application_new/global/widgets/main_btn.dart';
 import 'package:flutter_application_new/global/widgets/main_btn2.dart';
 import 'package:flutter_application_new/modules/mafia/cubit/mafia_cubit.dart';
 import 'package:flutter_application_new/modules/mafia/models/mafia_role_model.dart';
+import 'package:flutter_application_new/modules/mafia/widgets/mafia_show_role_widget.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
 class MafiaView extends StatelessWidget {
@@ -20,6 +20,7 @@ class MafiaView extends StatelessWidget {
       child: Scaffold(
         body: SafeArea(
             child: Container(
+              height: double.infinity,
               decoration: const BoxDecoration(
                 gradient: LinearGradient(
                   begin: Alignment.topCenter,
@@ -36,42 +37,55 @@ class MafiaView extends StatelessWidget {
                     builder: (context) {
                       return BlocBuilder<MafiaCubit, MafiaState>(
                         builder: (context, state) {
-                          return SingleChildScrollView(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Text(
-                                  "افراد به صورت نوبتی با کلیک بروی دکمه پایین نقششون رو  به شکل رندوم مشخص می کنند",
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(fontSize: 16.sp,
-                                      fontWeight: FontWeight.bold,
-                                      color: UiColors.whiteColor),
-                                ),
-                                SvgPicture.asset(Assets.images.svg.mafiaSade),
-
-                                SizedBox(
-                                  height: AppDistances.small4.w,
-                                ),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    MainButton(
-                                      btnText: "نقش من رو بگو!",
-                                      onPress: () {},
-                                      fontsize: 15.sp,
-                                    ),
-                                    SizedBox(
-                                      width: AppDistances.small4.w,
-                                    ),
-                                    MainButton2(
-                                      btnText: "نقش  رو بپوشون!",
-                                      onPress: () {},
-                                      fontsize: 15.sp,
+                          final mafiaCubit = BlocProvider.of<MafiaCubit>(context);
+                          return Center(
+                            child: SingleChildScrollView(
+                              child: Column(
+                                children: [
+                                  Text(
+                                    "افراد به صورت نوبتی با کلیک بروی دکمه پایین نقششون رو  به شکل رندوم مشخص می کنند",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(fontSize: 16.sp,
+                                        fontWeight: FontWeight.bold,
+                                        color: UiColors.whiteColor),
+                                  ),
+                                  if(state is MafiaInitial)...[
+                                    Image.asset(Assets.images.png.mafia.path)
+                                  ] else if(state is MafiaChangeRoleState)...[
+                                    MafiaShowRoleWidget(role: state.role)
+                                  ] else if(state is MafiaHideRoleState)...[
+                                    Text(
+                                      "برای دیدن نقشت ضربه بزن!",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(fontSize: 16.sp,
+                                          fontWeight: FontWeight.bold,
+                                          color: UiColors.whiteColor),
                                     ),
                                   ],
-                                )
-                              ],
+
+                                  SizedBox(
+                                    height: AppDistances.small4.w,
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      MainButton(
+                                        btnText: "نقش من رو بگو!",
+                                        onPress: () => mafiaCubit.changeRole(),
+                                        fontsize: 15.sp,
+                                      ),
+                                      SizedBox(
+                                        width: AppDistances.small4.w,
+                                      ),
+                                      MainButton2(
+                                        btnText: "نقش  رو بپوشون!",
+                                        onPress: () => mafiaCubit.hideMafiaRole(),
+                                        fontsize: 15.sp,
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
                             ),
                           );
                         },
--- lib/modules/mafia/widgets/mafia_show_role_widget.dart
+++ lib/modules/mafia/widgets/mafia_show_role_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_new/gen/assets.gen.dart';
 import 'package:flutter_application_new/global/utils/constants/ui_colors.dart';
 import 'package:flutter_application_new/modules/mafia/models/mafia_role_model.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
 class MafiaShowRoleWidget extends StatelessWidget {
@@ -11,7 +11,7 @@ class MafiaShowRoleWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Image.asset(Assets.images.png.mafia.path),
+        SvgPicture.asset(role.imagePath),
         Text(
           role.roleName,
           style: TextStyle(fontSize: 16.sp,
