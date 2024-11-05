43d35fd5a8a3a7cf878ae7886e5b808fc92595d2
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-11-05 09:02:43 -0800

    completed MafiaView ui

---
+++ lib/modules/mafia/view/mafia_view.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_new/gen/assets.gen.dart';
+import 'package:flutter_application_new/global/utils/constants/app_distances.dart';
+import 'package:flutter_application_new/global/utils/constants/ui_colors.dart';
+import 'package:flutter_application_new/global/widgets/main_btn.dart';
+import 'package:flutter_application_new/global/widgets/main_btn2.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+
+class MafiaView extends StatelessWidget {
+  const MafiaView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  UiColors.darkBlueColor3,
+                  UiColors.darkBlueColor5,
+                ],
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(AppDistances.pageBdyMargin),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                      "افراد به صورت نوبتی با کلیک بروی دکمه پایین نقششون رو  به شکل رندوم مشخص می کنند",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 16.sp,fontWeight: FontWeight.bold,color: UiColors.whiteColor),
+                  ),
+                  SvgPicture.asset(Assets.images.svg.mafiaSade),
+                  Text(
+                    "مافیا ساده",
+                    style: TextStyle(fontSize: 16.sp,fontWeight: FontWeight.bold,color: UiColors.whiteColor),
+                  ),
+                  SizedBox(
+                    height: AppDistances.small4.w,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      MainButton(
+                        btnText: "نقش من رو بگو!",
+                        onPress: () {
+                        },
+                        fontsize: 15.sp,
+                      ),
+                       SizedBox(
+                        width: AppDistances.small4.w,
+                      ),
+                      MainButton2(
+                        btnText: "نقش  رو بپوشون!",
+                        onPress: () {
+                        },
+                        fontsize: 15.sp,
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          )
+      ),
+    );
+  }
+}
