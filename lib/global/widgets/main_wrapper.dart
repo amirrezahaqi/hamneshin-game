50a29746d2ea3641ef1f8b1dc006c247bd2dfb44
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-11-21 07:34:34 -0800

    impl NavigationCubit class and completed app navigation

--- lib/global/widgets/main_wrapper.dart
+++ lib/global/widgets/main_wrapper.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_new/global/cubits/navigation_cubit.dart';
 import 'package:flutter_application_new/global/utils/constants/ui_colors.dart';
 import 'package:flutter_application_new/global/widgets/app-bar.dart';
 import 'package:flutter_application_new/global/widgets/bottom-navigation.dart';
 import 'package:flutter_application_new/modules/help/views/help_view.dart';
 import 'package:flutter_application_new/modules/home/view/home_view.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
 class MainWrapper extends StatelessWidget {
@@ -23,41 +25,51 @@ class MainWrapper extends StatelessWidget {
             ],
           ),
         ),
-        child: Stack(
-          children: [
-            Positioned.fill(
-              child: Padding(
-                padding:  EdgeInsets.only(bottom: 35.w,top: 16.w),
-                child: const IndexedStack(
-                  index: 0,
-                  children: [
-                    HomeView(),
-                    HelpView(),
-                    SizedBox(),
-                  ],
-                ),
-              ),
-            ),
+        child: BlocProvider(
+          create: (context) => NavigationCubit(),
+          child: BlocBuilder<NavigationCubit, int>(
+            builder: (context, state) {
+              final navCubit = BlocProvider.of<NavigationCubit>(context);
+              return Stack(
+                children: [
+                  Positioned.fill(
+                    child: Padding(
+                      padding: EdgeInsets.only(bottom: 35.w, top: 16.w),
+                      child: IndexedStack(
+                        index: state,
+                        children: const [
+                          HomeView(),
+                          HelpView(),
+                          SizedBox(),
+                        ],
+                      ),
+                    ),
+                  ),
 
-            // navigation
-            Positioned(
-              bottom: 4.w,
-                left: 0,
-                right: 0,
-                child: const BottomNavigation()
-            ),
+                  // navigation
+                  Positioned(
+                      bottom: 4.w,
+                      left: 0,
+                      right: 0,
+                      child: BottomNavigation(onPageChange: (pageIndex) {
+                        navCubit.changePage(pageIndex);
+                      },)
+                  ),
 
-            // app bar
-            Positioned(
-                top: 4.w,
-                left: 0,
-                right: 0,
-                child: SizedBox(
-                  height: 8.h,
-                    child: const AppBarWidget())
-            ),
+                  // app bar
+                  Positioned(
+                      top: 4.w,
+                      left: 0,
+                      right: 0,
+                      child: SizedBox(
+                          height: 8.h,
+                          child: const AppBarWidget())
+                  ),
 
-          ],
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
