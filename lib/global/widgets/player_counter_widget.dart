d241b5dd7e4bc891ee27cb0d667eac9f4251fb64
amirrezahaqi <amirrezahaqi@gmail.com>
2024-11-21 19:32:09 +0330

    new commit

--- lib/global/widgets/player_counter_widget.dart
+++ lib/global/widgets/player_counter_widget.dart
@@ -7,14 +7,17 @@ class PlayerCounterWidget extends StatelessWidget {
       {super.key,
       required this.playerCounts,
       required this.onItemTap,
-      this.selectedItem = 0});
+      this.selectedItem = 0, this.textController});
   final List<int> playerCounts;
-  final Function(int number) onItemTap;
+  final Function(int number , int indexItem) onItemTap;
   final int selectedItem;
+  final TextEditingController? textController;
   @override
   Widget build(BuildContext context) {
     // text field widget
     final Widget textField = TextFormField(
+      readOnly: true,
+      controller: textController,
       textAlign: TextAlign.center,
       decoration: InputDecoration(
         contentPadding: EdgeInsets.symmetric(vertical: 2.w, horizontal: 3.w),
@@ -40,12 +43,12 @@ class PlayerCounterWidget extends StatelessWidget {
           final bool selectCondition =
               selectedItem == playerCounts.indexOf(playerCounts[index]);
           return GestureDetector(
-            onTap: () => onItemTap(playerCounts[index]),
+            onTap: () => onItemTap(playerCounts[index],playerCounts.indexOf(playerCounts[index])),
             child: AnimatedContainer(
               duration: const Duration(milliseconds: 350),
               margin: EdgeInsets.all(2.w),
-              width: 12.w,
-              height: 12.w,
+              width: 10.w,
+              height: 10.w,
               decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(2.w),
                   border: Border.all(
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_new/global/widgets/app_view.dart';
-import 'package:flutter_application_new/modules/home/cubit/home_cubit.dart';
+import 'package:flutter_application_new/modules/cheshmak-marg/cubit/cheshmakmarg_cubit.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(
     BlocProvider(
-      create: (_) => HomeCubit(),
+      create: (_) => CheshmakmargCubit(),
       child: const AppView(),
     ),
   );
