9aa76f0f487dc0cfbd268174fca9cb4ca9bbd464
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-11-26 23:23:50 -0800

    add scaffold widget to splash view

--- lib/modules/splash/view/splash_view.dart
+++ lib/modules/splash/view/splash_view.dart
@@ -29,100 +29,102 @@ class _SplashViewState extends State<SplashView> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        padding: const EdgeInsets.all(AppDistances.pageBdyMargin),
-        width: double.infinity,
-        height: double.infinity,
-        decoration: const BoxDecoration(
-          gradient: RadialGradient(
-            center: Alignment.center,
-            radius: 1.0, // شعاع گرادیان
-            colors: [
-              UiColors.lightBlueColor6, // رنگ روشن‌تر در مرکز
-              UiColors.darkBlueColor6, // رنگ تیره‌تر در حاشیه
-            ],
-            stops: [
-              0.3, // روشنایی تا این نقطه
-              1.0, // تیره‌تر از این نقطه به بعد
-            ],
-          ),
-        ),
-        child: Padding(
-            padding: const EdgeInsets.only(top: AppDistances.large),
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                Positioned(
-                  top: 30.w,
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        width: 50.w,
-                        child: Image.asset(
-                          "assets/images/png/hamneshin.png",
-                          scale: 3,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 2.w,
-                      ),
-                      SizedBox(
-                        width: 50.w,
-                        child: Image.asset(
-                          "assets/images/png/splashText.png",
-                          scale: 1,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Positioned(
-                  bottom: 40.w,
-                  child: const Column(
-                    children: [
-                      SpinKitThreeBounce(
-                        color: UiColors.whiteColor,
-                      ),
-                      Text(
-                        StringConst.loaded,
-                        style: TextStyle(
-                            height: 1.5, // فاصله بین خطوط
-
-                            fontSize: 20,
-                            fontFamily: FontFamily.pelak,
-                            color: UiColors.whiteColor,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ],
-                  ),
-                ),
-                Positioned(
-                  bottom: 8.w,
-                  child: const Text(
-                    StringConst.created,
-                    style: TextStyle(
-                        height: 1.5, // فاصله بین خطوط
-
-                        fontSize: 15,
-                        fontFamily: FontFamily.pelak,
-                        color: UiColors.whiteColor,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Positioned(
-                  bottom: 2.w,
-                  child: const Text(
-                    StringConst.version,
-                    style: TextStyle(
-                        height: 1.5, // فاصله بین خطوط
-
-                        fontSize: 10,
-                        fontFamily: FontFamily.pelak,
-                        color: UiColors.whiteColor,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
+    return Scaffold(
+      body: Container(
+          padding: const EdgeInsets.all(AppDistances.pageBdyMargin),
+          width: double.infinity,
+          height: double.infinity,
+          decoration: const BoxDecoration(
+            gradient: RadialGradient(
+              center: Alignment.center,
+              radius: 1.0, // شعاع گرادیان
+              colors: [
+                UiColors.lightBlueColor6, // رنگ روشن‌تر در مرکز
+                UiColors.darkBlueColor6, // رنگ تیره‌تر در حاشیه
               ],
-            )));
+              stops: [
+                0.3, // روشنایی تا این نقطه
+                1.0, // تیره‌تر از این نقطه به بعد
+              ],
+            ),
+          ),
+          child: Padding(
+              padding: const EdgeInsets.only(top: AppDistances.large),
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Positioned(
+                    top: 30.w,
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          width: 50.w,
+                          child: Image.asset(
+                            "assets/images/png/hamneshin.png",
+                            scale: 3,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 2.w,
+                        ),
+                        SizedBox(
+                          width: 50.w,
+                          child: Image.asset(
+                            "assets/images/png/splashText.png",
+                            scale: 1,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 40.w,
+                    child: const Column(
+                      children: [
+                        SpinKitThreeBounce(
+                          color: UiColors.whiteColor,
+                        ),
+                        Text(
+                          StringConst.loaded,
+                          style: TextStyle(
+                              height: 1.5, // فاصله بین خطوط
+      
+                              fontSize: 20,
+                              fontFamily: FontFamily.pelak,
+                              color: UiColors.whiteColor,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 8.w,
+                    child: const Text(
+                      StringConst.created,
+                      style: TextStyle(
+                          height: 1.5, // فاصله بین خطوط
+      
+                          fontSize: 15,
+                          fontFamily: FontFamily.pelak,
+                          color: UiColors.whiteColor,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 2.w,
+                    child: const Text(
+                      StringConst.version,
+                      style: TextStyle(
+                          height: 1.5, // فاصله بین خطوط
+      
+                          fontSize: 10,
+                          fontFamily: FontFamily.pelak,
+                          color: UiColors.whiteColor,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ))),
+    );
   }
 }
